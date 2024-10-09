138a3cccfcba17e22a11030349afefb258d77c4f
Boobsmarck <141916531+Boobsmarck@users.noreply.github.com>
2024-10-10 01:17:58 +0200

    ship supply and malta+rivers
    rivers stronger ships eat less supply
    gibraltar malta cyprus +25% org regain

--- common/defines/hhm_defines.lua
+++ common/defines/hhm_defines.lua
@@ -123,8 +123,8 @@ NDefines.NMilitary.UNIT_DIGIN_CAP = 0
 NDefines.NMilitary.COMBAT_VALUE_ORG_IMPORTANCE = 0
 NDefines.NMilitary.COMBAT_VALUE_STR_IMPORTANCE = 0
 NDefines.NMilitary.NAVAL_INVASION_PLANNING_BONUS_MALUS = -0.5
-NDefines.NMilitary.RIVER_CROSSING_PENALTY = -0.25                 -- vanilla -0.30 | small river crossing
-NDefines.NMilitary.RIVER_CROSSING_PENALTY_LARGE = -0.425          -- vanilla -0.6 | large river crossing
+NDefines.NMilitary.RIVER_CROSSING_PENALTY = -0.35                 -- vanilla -0.30 | small river crossing
+NDefines.NMilitary.RIVER_CROSSING_PENALTY_LARGE = -0.60          -- vanilla -0.6 | large river crossing
 NDefines.NMilitary.PLANNING_MAX = 0.25                          	-- can get more from techs
  
 NDefines.NMilitary.LAND_COMBAT_STR_ARMOR_ON_SOFT_DICE_SIZE = 2   -- extra damage dice if our armor outclasses enemy
